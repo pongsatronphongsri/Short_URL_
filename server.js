31ecfeb2a566266093fad0580b715579ceeb8c9d
Phongsatorn Phongsri <s6306021622011@email.kmutnb.ac.th>
2023-09-07 20:49:18 +0700

    update

--- server.js
+++ server.js
@@ -14,8 +14,12 @@ mongoose.set('strictQuery', false)
     }
 }
 module.exports = connectDB*/
-mongoose.connect(process.env.MONGODB_CONNECT_URI)
-   
+mongoose.connect(process.env.MONGODB_CONNECT_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+})
+    
     .then(()=> console.log('connection successfullly'))
     .catch((err)=>console.error(err))
     
